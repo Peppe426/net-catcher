89ab2813ac1994fa26a974b1c617b15df31daed6
Peter L <61692826+PeterLAxakon@users.noreply.github.com>
2023-05-29 09:57:41 +0200

    feat(OpenTelemetryExtensions) added support for Azure MonitorTraceExporter

---
+++ net-catcher/CatchSubscriber/Extentions/OpenTelemetryAzureExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Monitor.OpenTelemetry.Exporter;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+public static partial class OpenTelemetryExtensions
+{
+    public static void AddOpenTelemetryForAzure(this IServiceCollection services, string serviceName, string serviceVersion, string azureMonitorConnectionString)
+    {
+        services.AddOpenTelemetry()
+           .WithTracing(builder =>
+           {
+               builder
+               .AddSource(serviceName)
+               .AddConsoleExporter()
+               .AddAspNetCoreInstrumentation()
+               .AddHttpClientInstrumentation()
+               .AddAzureMonitorTraceExporter(options =>
+                {
+                    options.ConnectionString = azureMonitorConnectionString;
+                })
+               .ConfigureResource(resource => resource.AddService(serviceName: serviceName, serviceVersion: serviceVersion));
+           });
+    }
+}
