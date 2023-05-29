94381fda81cf7bdd84a4ec8d1a63e13e4e12f5b7
Peter L <61692826+PeterLAxakon@users.noreply.github.com>
2023-05-29 16:41:18 +0200

    feat(OpenTelemetryExtensions) added AddOpenTelemetryForConsole
    Co-Authored-By: Peter L <61692826+PeterLAxakon@users.noreply.github.com>
    Co-Authored-By: Bar <113115865+Barbarossa426@users.noreply.github.com>

---
+++ net-catcher/CatchSubscriber/Extentions/OpenTelemetryConsoleExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace CatchSubscriber.Extentions;
+
+public static partial class OpenTelemetryExtensions
+{
+    public static void AddOpenTelemetryForConsole(this IServiceCollection services, string serviceName, string serviceVersion)
+    {
+        services.AddOpenTelemetry()
+           .WithTracing(builder =>
+           {
+               builder
+               .AddSource(serviceName)
+               .AddAspNetCoreInstrumentation()
+               .AddHttpClientInstrumentation()
+               .AddConsoleExporter()
+               .ConfigureResource(resource => resource.AddService(serviceName: serviceName, serviceVersion: serviceVersion));
+           });
+    }
+}
