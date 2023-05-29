b2fefc7618df0fdc3ac244f8accca7ec7a5da056
Peter L <61692826+PeterLAxakon@users.noreply.github.com>
2023-05-29 14:24:58 +0200

    refactor(ErrorProcessors) update and added email suport
    update errorProcessors and init support for using email as a processor
    
    Co-Authored-By: Peter L <61692826+PeterLAxakon@users.noreply.github.com>
    Co-Authored-By: Bar <113115865+Barbarossa426@users.noreply.github.com>

--- net-catcher/CatchSubscriber/Interfaces/IErrorProcesser.cs
+++ net-catcher/CatchSubscriber/Interfaces/IErrorProcesser.cs
@@ -5,7 +5,7 @@ namespace CatchSubscriber.Interfaces;
 
 public interface IErrorProcesser
 {
-    Task ProcessError(string message, LogLevel logLevel, List<CatchAction>? actions = null);
+    Task ProcessError(string message, LogLevel logLevel, List<CatchAction>? actions = null, LogLevel level = LogLevel.Critical);
 
     ErrorProcessor RegisterSlack(string hookUrl, string channel, string userName, string emoji = "");
 }
