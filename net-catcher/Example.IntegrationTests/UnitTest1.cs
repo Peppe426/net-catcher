35e4132f1e269e08c6f3a6dae5adb938c6b7960d
Peter L <61692826+PeterLAxakon@users.noreply.github.com>
2023-05-31 15:21:01 +0200

    feat(example) added integrationtest project
    Co-Authored-By: Bar <113115865+barbarossa426@users.noreply.github.com>
    Co-Authored-By: Peter Liljeholm <45904769+Peppe426@users.noreply.github.com>

---
+++ net-catcher/Example.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Example.IntegrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
