229d5d51877c1e385f43fb225a0df90e9deccea8
Peter L <61692826+PeterLAxakon@users.noreply.github.com>
2023-05-31 15:48:54 +0200

    test(integrationtest) added base implementation
    Co-Authored-By: Bar <113115865+barbarossa426@users.noreply.github.com>
    Co-Authored-By: Peter Liljeholm <45904769+Peppe426@users.noreply.github.com>

---
+++ net-catcher/Example.IntegrationTests/Base/IntegrationTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.IntegrationTests.Base;
+
+public class IntegrationTest : IDisposable
+{
+    private TestWebApplicationFactory _appFactory = default!;
+    public IServiceProvider ServiceProvider = default!;
+
+    protected HttpClient HttpClient { get; private set; } = default!;
+
+    public IntegrationTest()
+    {
+        _appFactory = new TestWebApplicationFactory();
+        HttpClient = _appFactory.CreateClient();
+        ServiceProvider = _appFactory.Services;
+    }
+
+    protected AsyncServiceScope GetRequiredService<T>(out T service) where T : notnull
+    {
+        var scope = ServiceProvider.CreateAsyncScope();
+
+        try
+        {
+            service = scope.ServiceProvider.GetRequiredService<T>();
+        }
+        catch
+        {
+            scope.Dispose();
+            throw;
+        }
+
+        return scope;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _appFactory.Dispose();
+        }
+    }
+}
---
+++ net-catcher/Example.IntegrationTests/Base/TestWebApplicationFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Example.IntegrationTests.Base;
+
+public class TestWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+        });
+
+        return base.CreateHost(builder);
+    }
+}
