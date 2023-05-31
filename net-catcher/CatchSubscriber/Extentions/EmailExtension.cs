0bcc32acd2d2144625a072fd8b754a416fbbfda3
Peter L <61692826+PeterLAxakon@users.noreply.github.com>
2023-05-31 15:18:46 +0200

    feat(emailProcessor) added

---
+++ net-catcher/CatchSubscriber/Extentions/EmailExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CatchSubscriber.Extentions;
+
+public static class EmailExtension
+{
+    /// <summary>
+    /// Injects <see href="https://github.com/lukencode/FluentEmail">FluentEmail</see> using IServiceCollection
+    /// </summary>
+    /// <param name="host">SMTP host</param>
+    /// <param name="port">SMTP port</param>
+    /// <param name="username"><see cref='System.Net.NetworkCredential'>NetworkCredential</see> username</param>
+    /// <param name="password"><see cref='System.Net.NetworkCredential'>NetworkCredential</see> password</param>
+    /// <param name="defaultFromEmail">FluentEmail default sender</param>
+    /// <param name="defaultFromName">FluentEmail default sender name</param>
+    public static void AddEmail(this IServiceCollection services, string host, int port, string username, string password, string defaultFromEmail, string defaultFromName = "")
+    {
+        services
+        .AddFluentEmail(defaultFromEmail, defaultFromName)
+        .AddSmtpSender(host, port, username, password);
+    }
+}
